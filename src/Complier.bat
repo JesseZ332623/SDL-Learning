1ef9da6be6093205fd087ab31afdcc1875dd8e23
Jesse <3191955858@qq.com>
2024-08-30 21:42:19 +0800

    编译脚本

--- src/Complier.bat
+++ src/Complier.bat
@@ -1,13 +1,16 @@
 @echo off
 
 REM 源文件路径所在
-SET SourceCode="./*.c"
+SET SourceCode="./extra_server.cpp" "./defs_server.cpp"
 
 REM SDL2 接口路径所在 
 SET SDLHeadFile="F:\SDL\SDL2\include"
 
 REM SDL_Image 接口路径所在
 SET SDLImageHeadFile="F:\SDL\SDL2_image\include"
+
+REM SDL_Net 接口路径所在
+SET SDL_NetHeadFile="F:\SDL\SDL2_net\include"
 
 REM PCG 随机数生成器的接口路径所在
 SET PCGHeadFile="F:\pcg-c-basic\include"
@@ -20,10 +23,18 @@ REM SDL_Image 的库文件，库名
 SET SDLImage_LibPath="F:\SDL\SDL2_image\lib\x64"
 SET SDLImage_LibName="SDL2_image"
 
+REM SDL_Net 的库文件，库名
+SET SDLNet_LibPath="F:\SDL\SDL2_net\lib\x64"
+SET SDLNet_LibName="SDL2_net"
+
 REM PCG 随机数生成器的库文件路径所在
 SET PCG_LibPath="F:\pcg-c-basic\lib\pcg_basic.o"
 
 REM 可执行文件路径
-SET ExecutionFilePath="../../bin/05.exe"
+SET ExecutionFilePath="../../bin/extra_server.exe"
 
-gcc %SourceCode% %PCG_LibPath% -I%SDLHeadFile% -I%SDLImageHeadFile% -I%PCGHeadFile% -L%SDL_LibPath% -L%SDLImage_LibPath%  -o %ExecutionFilePath% -O3 -Wall -l%SDL_LibName% -l%SDLImage_LibName% -std=c23
+SET ALL_INCLUDE=-I%SDLHeadFile% -I%SDLImageHeadFile% -I%PCGHeadFile% -I%SDL_NetHeadFile%
+SET ALL_LIB=-L%SDL_LibPath% -L%SDLImage_LibPath% -L%SDLNet_LibPath%
+SET ALL_LIBNAME=-l%SDL_LibName% -l%SDLImage_LibName% -l%SDLNet_LibName%
+
+g++ %SourceCode% %PCG_LibPath% %ALL_INCLUDE% %ALL_LIB% -o %ExecutionFilePath% -O3 -Wall %ALL_LIBNAME% -std=c++23
