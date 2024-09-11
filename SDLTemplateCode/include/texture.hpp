5c1cd8f0088d07afed4452a54a703b483cada3ba
Jesse <3191955858@qq.com>
2024-09-11 23:03:33 +0800

    一些 SDL 纹理封装类的声明和实现

---
+++ SDLTemplateCode/include/texture.hpp
@@ -0,0 +1,169 @@
+#include "SDL.h"
+#include "SDL_image.h"
+
+#include <string>
+
+/**
+ * @brief 封装了 SDL 纹理的基类，声明对于纹理的一些通用操作。 
+*/
+class TextureBisic
+{
+    private:
+        SDL_Texture * texture;      // SDL 纹理
+        SDL_Rect    renderPosition; // 纹理的原始长宽，以及在屏幕中的渲染位置
+    
+    public:
+        TextureBisic(void) : texture(nullptr), renderPosition({0}) {}
+
+        /**
+         * @brief 设置 SDL 纹理。
+        */
+        void setTexture(SDL_Texture * __t) { this->texture = __t; }
+
+        /**
+         * @brief 获取 SDL 纹理。
+        */
+        SDL_Texture * getTexture(void) const { return this->texture; }
+
+        /**
+         * @brief 获取该纹理的原始长宽，以及该纹理渲染在屏幕的位置。
+        */
+        SDL_Rect getRenderPosition(void) const { return this->renderPosition; }
+        SDL_Rect & getRenderPosition(void) { return this->renderPosition; }
+
+        /**
+         * @brief 销毁该纹理，并重置所有数据成员。 
+        */
+        virtual ~TextureBisic();
+};
+
+/**
+ * @brief 图片纹理类，用于加载渲染图片纹理。
+*/
+class TextureImage : public TextureBisic
+{
+    private:
+        std::string textureName;
+        SDL_Rect    clipPosition;
+
+        /**
+         * @brief 设置纹理名
+        */
+        void setTextureName(std::string __path);
+
+    public:
+        TextureImage(void) : TextureBisic(), textureName(), clipPosition({0}) {}
+
+        /**
+         * @brief 获取纹理的裁剪位置和裁剪长宽。
+        */
+        SDL_Rect getClipPosition(void) const { return this->clipPosition; }
+        SDL_Rect & getClipPosition(void) { return this->clipPosition; }
+
+        /**
+         * @brief 设置纹理的颜色值（RGB）
+        */
+        void setColor(SDL_Color __color) {
+            SDL_SetTextureColorMod(this->getTexture(), __color.r, __color.g, __color.b);
+        }
+
+        /**
+         * @brief 设置纹理的混合模式。
+        */
+        void setBlendMode(SDL_BlendMode __blending) {
+            SDL_SetTextureBlendMode(this->getTexture(), __blending);
+        }
+
+        /**
+         * @brief 设置纹理的 Alpha 通道值（透明度 Tansparency）。
+        */
+        void setAlpha(Uint8 __alpha) { 
+            SDL_SetTextureAlphaMod(this->getTexture(), __alpha);
+        }
+
+        /**
+         * @brief 加载一张图片的纹理信息。
+         * 
+         * @param __path         图片路径
+         * @param __render       渲染器
+         * 
+         * @return 是否加载成功
+        */
+        bool load(std::string __path, SDL_Renderer * __render);
+
+        /**
+         * @brief 指定纹理相对于屏幕的渲染平面坐标（x, y）和 裁剪范围，并交由渲染器渲染。
+        */
+        void render(int __x, int __y, SDL_Rect __clips, SDL_Renderer * __render);
+
+        ~TextureImage();
+};
+
+/**
+ * @brief 矩形纹理类，用于加载和渲染一个矩形纹理 
+*/
+class RectengleTexture : public TextureBisic
+{
+    private:
+        std::string rectengleName;
+
+    public:
+        /**
+         * @brief 加载一个指定长宽的矩形 __name。
+        */
+        bool load(std::string __name, int __w, int __h, SDL_Renderer * __render);
+
+        /**
+         * @brief 把加载好的颜色（RGBA）为 __color 的矩形纹理渲染在屏幕的 (__x, __y) 处。
+        */
+        void render(int __x, int __y, SDL_Color __color, SDL_Renderer * __render);
+
+        ~RectengleTexture();
+};
+
+/**
+ * @brief 圆形纹理类，这个类的实现会比较特殊，
+ *        不需要依赖 SDL_Texture，而是通过渲染像素点的方式直接绘制出整个圆。
+*/
+class CircleTexture
+{
+    public:
+        struct CircleInfo {
+            int centerX, centerY;   // 圆心坐标
+            int radius;             // 圆半径
+        };
+
+        /**
+         * @brief 判断点 (__x, __y) 是否在这个圆（包括圆上）之内。
+         *        方法很简单，求该点与圆形的欧几里得距离 d，如果大于圆半径就意味着不在圆上。
+        */
+        static bool isInCircle(int __x, int __y, CircleInfo & __circleInfo);
+
+        /**
+         * @brief 显示圆的相关数据。
+        */
+        void showCircleInfo(void);
+
+    private:
+        std::string circleName;     // 圆的编号
+        CircleInfo  circleInfo;     // 圆的属性
+    
+    public:
+        /**
+         * @brief 设置该圆的编号和圆的属性。
+        */
+        void load(const std::string __name, CircleInfo __circleInfo);
+
+        /**
+         * @brief 在圆上寻找 __segments 个点并渲染（__segments 的值越大，圆轮廓越平滑）。
+         *        方法很简单，使用圆的标准方程就能求出圆上任意一点。
+        */
+        void render(int __segments, SDL_Color __color, SDL_Renderer * __render);
+
+        /**
+         * @brief 将圆用 __color 颜色填充。
+        */
+        void fill(SDL_Color __color, SDL_Renderer * __render);
+
+        ~CircleTexture();
+};
---
+++ SDLTemplateCode/src/texture.cpp
@@ -0,0 +1,235 @@
+#include "../include/texture.hpp"
+
+#include <cstring>
+#include <cmath>
+#include <MyLib/myLogerDef.h>
+
+TextureBisic::~TextureBisic()
+{
+    if (this->getTexture() != nullptr) {
+        SDL_DestroyTexture(this->texture);
+        this->texture = nullptr;
+    }
+}
+
+void TextureImage::setTextureName(std::string __path)
+{
+    std::size_t pos = __path.rfind('/');
+
+    if (pos != std::string::npos) { 
+        this->textureName = __path.substr(pos + 1); 
+    }
+    else { this->textureName = __path; }
+}
+
+bool TextureImage::load(std::string __path, SDL_Renderer * __render)
+{
+    using namespace MyLib::MyLoger;
+
+    SDL_Texture * finalTexture = nullptr;
+
+    NOTIFY_LOG("Load image [" + __path + "]\n");
+    SDL_Surface * loadSurface = IMG_Load(__path.c_str());
+
+    if (!loadSurface) 
+    {
+        ERROR_LOG("Unable to load image: " + __path + 
+                  " SDL Image ERROR: " + std::string(IMG_GetError()) + '\n'
+        );
+    }
+    else
+    {
+        this->setTextureName(__path);
+#if true
+        SDL_SetColorKey(
+            loadSurface, SDL_TRUE, 
+            SDL_MapRGB(loadSurface->format, 0x00, 0xFF, 0xFF)
+        );
+#endif
+
+        finalTexture = SDL_CreateTextureFromSurface(__render, loadSurface);
+
+        if (!finalTexture) {
+            ERROR_LOG(
+                "Unable to create texture from " + __path + 
+                " SDL ERROR: " + std::string(SDL_GetError()) + '\n'
+            );
+        }
+        else {
+            this->getRenderPosition().w = loadSurface->w;
+            this->getRenderPosition().h = loadSurface->h;
+        }
+
+        SDL_FreeSurface(loadSurface);
+    }
+
+    this->setTexture(finalTexture);
+
+    return (this->getTexture() != nullptr);
+}
+
+void TextureImage::render(int __x, int __y, SDL_Rect __clips, SDL_Renderer * __render)
+{
+    this->getRenderPosition().x = __x;
+    this->getRenderPosition().y = __y;
+    std::memcpy(&this->getClipPosition(), &__clips, sizeof(SDL_Rect));
+
+    SDL_Rect tempRect = {
+        this->getRenderPosition().x, this->getRenderPosition().y,
+        __clips.w, __clips.h
+    };
+
+    SDL_RenderCopy(
+        __render, this->getTexture(), 
+        &this->getClipPosition(), &tempRect
+    );
+}
+
+TextureImage::~TextureImage()
+{
+    using namespace MyLib::MyLoger;
+
+    CORRECT_LOG("Destory image texture [" + this->textureName + "]\n");
+}
+
+bool RectengleTexture::load(
+    std::string __name, int __w, int __h, SDL_Renderer * __render)
+{
+    using namespace MyLib::MyLoger;
+
+    NOTIFY_LOG(
+        "Load rectangle, w = " + std::to_string(__w) + 
+        " h = " + std::to_string(__h) + '\n'
+    );
+
+    SDL_Texture * rectangleTexture = SDL_CreateTexture(
+        __render, SDL_PIXELFORMAT_RGB888, 
+        SDL_TEXTUREACCESS_STATIC, __w, __h
+    );
+
+    if (!rectangleTexture)
+    {
+        ERROR_LOG(
+            "loadRectangle(): Unable to create texture, SDL ERROR: " + 
+            std::string(SDL_GetError()) + '\n'
+        );
+    }
+    else {
+        this->rectengleName = __name;
+        this->getRenderPosition().w = __w;
+        this->getRenderPosition().h = __h;
+    }
+
+    this->setTexture(rectangleTexture);
+
+    return (this->getTexture() != nullptr);
+}
+
+void RectengleTexture::render(
+    int __x, int __y, SDL_Color __color, SDL_Renderer * __render)
+{
+    this->getRenderPosition().x = __x;
+    this->getRenderPosition().y = __y;
+
+    SDL_SetRenderDrawColor(
+        __render, __color.r, __color.g, __color.b, 0xFF
+    );
+    SDL_RenderFillRect(__render, &this->getRenderPosition());
+}
+
+RectengleTexture::~RectengleTexture()
+{
+    using namespace MyLib::MyLoger;
+
+    CORRECT_LOG(
+        "Destory rectengle texture: [" + this->rectengleName + "]\n"
+    );
+}
+
+bool CircleTexture::isInCircle(int __x, int __y, CircleInfo & __circleInfo)
+{
+    return (std::pow(__x - __circleInfo.centerX, 2) + 
+           std::pow(__y - __circleInfo.centerY, 2)) < std::pow(__circleInfo.radius, 2);
+}
+
+void CircleTexture::showCircleInfo(void)
+{
+    using namespace MyLib::MyLoger;
+
+    printf(
+        "Circle name: %s\n"
+        "Center point = (%d, %d), radius = %d\n",
+        this->circleName.c_str(), 
+        this->circleInfo.centerX, this->circleInfo.centerY, 
+        this->circleInfo.radius
+    );
+}
+
+void CircleTexture::load(
+    const std::string __name, CircleInfo __circleInfo)
+{
+    using namespace MyLib::MyLoger;
+
+    this->circleName = __name;
+
+    NOTIFY_LOG("Load circle: [" + this->circleName + "]\n");
+
+    std::memcpy(&this->circleInfo, &__circleInfo, sizeof(CircleInfo));
+
+    this->showCircleInfo();
+}
+
+void CircleTexture::render(int __segments, SDL_Color __color, SDL_Renderer * __render)
+{
+    // 求每次渲染的角度增量
+    const float angleStep = 2.0f * M_PI / __segments;
+    float angle = 0.0f;
+
+    SDL_SetRenderDrawColor(__render, __color.r, __color.g, __color.b, 0xFF);
+
+    for (int count = 0; count <= __segments / 2; ++count)
+    {
+        /**
+         * 圆的标准方程，但是三角函数乘半径这部分要四舍五入后截断成整数。
+        */
+        int x = static_cast<int>(std::round(std::cos(angle) * circleInfo.radius)) + circleInfo.centerX;
+        int y = static_cast<int>(std::round(std::sin(angle) * circleInfo.radius)) + circleInfo.centerY;
+
+        /**
+         * 求圆上某一点的对称点也很容易。
+        */
+        int symmetryX = 2 * circleInfo.centerX - x;
+        int symmetryY = 2 * circleInfo.centerY - y;
+
+        SDL_RenderDrawPoint(__render, x, y);                    // 绘制圆上一点
+        SDL_RenderDrawPoint(__render, symmetryX, symmetryY);    // 绘制圆上一点的对称点
+
+        angle += angleStep; // 角度偏移量增加
+    }
+}
+
+void CircleTexture::fill(SDL_Color __color, SDL_Renderer * __render)
+{
+    SDL_SetRenderDrawColor(__render, __color.r, __color.g, __color.b, 0xFF);
+
+    for (int y = 0; y <= this->circleInfo.radius; ++y)
+    {
+        for (int x = 0; x <= this->circleInfo.radius; ++x)
+        {
+            if (this->isInCircle(x + circleInfo.centerX, y + circleInfo.centerY, this->circleInfo))
+            {
+                SDL_RenderDrawPoint(__render, x + circleInfo.centerX, y + circleInfo.centerY);
+                SDL_RenderDrawPoint(__render, x + circleInfo.centerX, -y + circleInfo.centerY);
+                SDL_RenderDrawPoint(__render, -x + circleInfo.centerX, y + circleInfo.centerY);
+                SDL_RenderDrawPoint(__render, -x + circleInfo.centerX, -y + circleInfo.centerY);
+            }
+        }
+    }
+}
+
+CircleTexture::~CircleTexture()
+{
+    using namespace MyLib::MyLoger;
+
+    CORRECT_LOG("Destory circle: [" + this->circleName + "]\n");
+}
