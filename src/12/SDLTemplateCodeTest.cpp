2c91de373c9ba70fe8d5ebc2c890636305707218
Jesse <3191955858@qq.com>
2024-09-12 21:45:28 +0800

    SDL 开发模板代码动态链接库测试用例

--- src/12/SDLTemplateCodeTest.cpp
+++ src/12/SDLTemplateCodeTest.cpp
@@ -4,23 +4,29 @@
 #define RECTENGLE_SIDE_LENGTH 250
 #define CIRCLE_RADIUS         RECTENGLE_SIDE_LENGTH / 2
 
+#define FRAME_WIDTH  64
+#define FAMRE_HEIGHT 205
+
 int WinMain(int argc, char const * argv[])
 {
     system("cls");
 
     SystemInit::WindowSize windowSize = {650, 650};
-    SystemInit      sysInit;
+    SystemInit      sysInit(windowSize, "12. SDL TemplateCode Test");
 
     EventsControl   eventsControl;
 
+    TextureImage        image;
     CircleTexture       circleA;
     RectengleTexture    rectengleA;
 
-    sysInit.init(windowSize, "12. SDL TemplateCode Test");
+    sysInit.init();
 
     int rectX = windowSize.w / 2 - RECTENGLE_SIDE_LENGTH / 2;
     int rectY = windowSize.h / 2 - RECTENGLE_SIDE_LENGTH / 2;
 
+    SDL_Rect framePos = {0, 0, FRAME_WIDTH, FAMRE_HEIGHT};
+    image.load("../img/11/stick_figuer.png", sysInit.getRenderer());
     rectengleA.load(
         "Rectengle A", 
         RECTENGLE_SIDE_LENGTH, RECTENGLE_SIDE_LENGTH, 
@@ -37,14 +43,15 @@ int WinMain(int argc, char const * argv[])
 
     while (!eventsControl.getRunstate())
     {
-        eventsControl.eventsRecord();
+        eventsControl.recordEvents();
 
         SDL_SetRenderDrawColor(sysInit.getRenderer(), 0xFF, 0xFF, 0xFF, 0xFF);
         SDL_RenderClear(sysInit.getRenderer());
 
+        image.render(0, 0, framePos, sysInit.getRenderer());
         rectengleA.render(
-            rectX, rectY, 
-            {0x65, 0xCB, 0xF6}, sysInit.getRenderer()
+            rectX, rectY, {0x65, 0xCB, 0xF6}, 
+            sysInit.getRenderer(), RectengleTexture::WHOLE
         );
         circleA.render(0xFFFF, {0x00, 0XAD, 0x12}, sysInit.getRenderer());
         circleA.fill({0x00, 0XAD, 0x12}, sysInit.getRenderer());
