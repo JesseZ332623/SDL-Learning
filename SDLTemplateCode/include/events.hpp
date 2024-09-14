cbb47c5ef088f63ad0b35464f2bb703064019a3a
Jesse <3191955858@qq.com>
2024-09-14 21:29:39 +0800

    SDL 事件的封装类

--- SDLTemplateCode/include/events.hpp
+++ SDLTemplateCode/include/events.hpp
@@ -1,4 +1,9 @@
+#ifndef __EVENTS_H__
+#define __EVENTS_H__
+
 #include "SDL_events.h"
+
+#include <unordered_map>
 
 /**
  * @brief SDL 事件的封装类，
@@ -6,12 +11,22 @@
  */
 class EventsControl
 {
+    public:
+        static int KeyCount;    // SDL 能记录到的键位总数
+        typedef std::unordered_map<SDL_Scancode, bool> KeyCodeMap;
     private:
         SDL_Event events;   // SDL 事件类
+
         bool runState;      // 程序是否运行标志位
+
+        /**
+         * @brief 一张记录键盘事件的哈希表，
+         *        其中 Key 代表某个键对应 SDL 内部的键码，Value 为布尔类型，表示该键是否被按下。
+        */
+        KeyCodeMap keyboardState;
     
     public:
-        EventsControl(void) : events(), runState(false) {}
+        EventsControl(void) : events(), runState(false), keyboardState() {}
 
         /**
          * @brief 获取程序的运行状态，确定合适退出程序。
@@ -22,8 +37,15 @@ class EventsControl
         bool getRunstate(void) const { return this->runState; }
 
         /**
+         * @brief 获取当前键盘的状态。
+        */
+        const KeyCodeMap & getKeyboardState(void) const { return this->keyboardState; };
+
+        /**
          * @brief 事件记录，
          *        调用 SDL_PollEvent() 探测事件并改变相应的标志位。
         */
         void recordEvents(void);
 };
+
+#endif // __EVENTS_H__
--- SDLTemplateCode/src/events.cpp
+++ SDLTemplateCode/src/events.cpp
@@ -1,11 +1,36 @@
 #include "../include/events.hpp"
 
+#include "../include/fmtTime.hpp"
+
+int EventsControl::KeyCount = SDL_NUM_SCANCODES;
+
 void EventsControl::recordEvents(void)
 {
+    using namespace fmt;
+
+#define CODE (this->events.key.keysym.scancode)
+
     while (SDL_PollEvent(&this->events))
     {
-        if (this->events.type == SDL_QUIT) {
-            this->runState = true;
+        switch (this->events.type)
+        {
+            case SDL_QUIT:
+                this->runState = true;
+                break;
+            
+            case SDL_KEYDOWN:
+                if (this->keyboardState.find(CODE) != this->keyboardState.end()) {
+                    this->keyboardState.at(CODE) = true;
+                }
+                else { this->keyboardState.insert({CODE, true}); }
+                break;
+            
+            case SDL_KEYUP:
+                this->keyboardState.at(CODE) = false;
+                break;
+
+            default:
+                break;
         }
     }
 }
