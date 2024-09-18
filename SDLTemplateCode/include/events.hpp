d8282713aa1801db01838a6113856951d92e5253
Jesse <3191955858@qq.com>
2024-09-18 22:09:40 +0800

    SDL 事件的封装类（新增鼠标事件）

--- SDLTemplateCode/include/events.hpp
+++ SDLTemplateCode/include/events.hpp
@@ -12,8 +12,9 @@
 class EventsControl
 {
     public:
-        static int KeyCount;    // SDL 能记录到的键位总数
+        static int KeyCount;    // SDL 能记录到的键盘键位总数
         typedef std::unordered_map<SDL_Scancode, bool> KeyCodeMap;
+        typedef std::unordered_map<decltype(SDL_MouseButtonEvent::button), bool> MouseButtonMap;
     private:
         SDL_Event events;   // SDL 事件类
 
@@ -21,15 +22,43 @@ class EventsControl
 
         /**
          * @brief 一张记录键盘事件的哈希表，
-         *        其中 Key 代表某个键对应 SDL 内部的键码，Value 为布尔类型，表示该键是否被按下。
+         *        其中 Key 代表某个键对应 SDL 内部的键码，
+         *        Value 为布尔类型，表示该键是否被按下。
         */
         KeyCodeMap keyboardState;
-    
-    public:
-        EventsControl(void) : events(), runState(false), keyboardState() {}
 
         /**
-         * @brief 获取程序的运行状态，确定合适退出程序。
+         * @brief 一张记录鼠标事件的哈希表，
+         *        其中 Key 代表鼠标上某个键对应 SDL 内部的键码，
+         *        Value 为布尔类型，表示该键是否被按下。
+        */
+        MouseButtonMap mouseButtonState;
+
+        bool mouseMotionState;  // 鼠标的运动状态
+
+        /**
+         * @brief 记录在某一帧下鼠标位于窗口的坐标。
+        */
+        SDL_Point mousePosition;
+
+        /**
+         * @brief 每隔一小段时间检查鼠标的位置，由此判断鼠标是否停止。
+         * 
+         * @param __interval 每隔多少毫秒检测一次（默认 100 毫秒）
+         * @param __times    检测次数（默认 3 次）
+         * 
+         * @return true  鼠标已静止
+         * @return false 鼠标在移动
+        */
+        bool isMouseMotionless(const Uint32 __interval = 100, int __times = 3);
+    
+    public:
+        EventsControl(void) : 
+        events(), runState(false), keyboardState(), 
+        mouseButtonState(), mouseMotionState(false), mousePosition() {}
+
+        /**
+         * @brief 获取程序的运行状态，确定合适的时机来退出程序。
          * 
          * @return true     收到退出信号
          * @return false    未收到退出信号
@@ -37,9 +66,35 @@ class EventsControl
         bool getRunstate(void) const { return this->runState; }
 
         /**
-         * @brief 获取当前键盘的状态。
+         * @brief 获取当前键盘键位的状态。
         */
-        const KeyCodeMap & getKeyboardState(void) const { return this->keyboardState; };
+        const KeyCodeMap & getKeyboardState(void) const { 
+            return this->keyboardState; 
+        };
+
+        /**
+         * @brief 获取当前鼠标键位的状态。
+        */
+        const MouseButtonMap & getMouseState(void) const { 
+            return this->mouseButtonState; 
+        }
+
+        /**
+         * @brief 获取鼠标的运动状态
+         * 
+         * @return true  鼠标已静止
+         * @return false 鼠标在移动
+        */
+        bool getMouseMotionState(void) const {
+            return this->mouseMotionState;
+        }
+
+        /**
+         * @brief 获取当前的鼠标位置。
+        */
+        const SDL_Point getMousePosition(void) const { 
+            return this->mousePosition; 
+        }
 
         /**
          * @brief 事件记录，
--- SDLTemplateCode/src/events.cpp
+++ SDLTemplateCode/src/events.cpp
@@ -2,13 +2,55 @@
 
 #include "../include/fmtTime.hpp"
 
+#include "SDL_timer.h"
+
 int EventsControl::KeyCount = SDL_NUM_SCANCODES;
+
+bool EventsControl::isMouseMotionless(const Uint32 interval, int times)
+{
+    static Uint32       lastCheckTime = 0;
+    static SDL_Point    tempMousePos;
+    static int          counter = 0;
+
+    Uint32 currentTime = SDL_GetTicks();
+
+    if (currentTime - lastCheckTime >= interval) {
+        SDL_GetMouseState(&tempMousePos.x, &tempMousePos.y);
+        lastCheckTime = currentTime;
+
+        // 重置计数器
+        if (counter == 0) { counter = times; }
+
+        // 检查鼠标位置
+        if (tempMousePos.x == this->mousePosition.x && tempMousePos.y == this->mousePosition.y) 
+        {
+            --counter;
+            if (counter <= 0) { return true; }
+            } else {
+                counter = times;  // 位置变化，重置计数器
+            }
+        }
+
+        return false;
+}
 
 void EventsControl::recordEvents(void)
 {
     using namespace fmt;
 
-#define CODE (this->events.key.keysym.scancode)
+/**
+ * @brief 获取键盘中某个键按下或松开时，该键对应的键码。
+*/
+#define GET_KEYCODE       (this->events.key.keysym.scancode)
+
+/**
+ * @brief 获取鼠标中某个键按下或松开时，该键对应的键码。
+*/
+#define GET_MOUSE_KEYCODE (this->events.button.button)
+
+#define IF_KEYMAP_FOUND (this->keyboardState.find(GET_KEYCODE) != this->keyboardState.end())
+
+#define IF_MOUSEMAP_FOUND (this->mouseButtonState.find(GET_MOUSE_KEYCODE) != this->mouseButtonState.end())
 
     while (SDL_PollEvent(&this->events))
     {
@@ -18,19 +60,100 @@ void EventsControl::recordEvents(void)
                 this->runState = true;
                 break;
             
+            // 记录键盘中某个键被按下
             case SDL_KEYDOWN:
-                if (this->keyboardState.find(CODE) != this->keyboardState.end()) {
-                    this->keyboardState.at(CODE) = true;
+                if (IF_KEYMAP_FOUND) {
+                    this->keyboardState[GET_KEYCODE] = true;
                 }
-                else { this->keyboardState.insert({CODE, true}); }
+                else { this->keyboardState.insert({GET_KEYCODE, true}); }
+                break;
+
+            // 记录键盘中某个键回弹
+            case SDL_KEYUP:
+                this->keyboardState[GET_KEYCODE] = false;
+                break;
+
+            // 记录鼠标的某个按键按下
+            case SDL_MOUSEBUTTONDOWN:
+                if (IF_MOUSEMAP_FOUND) {
+                    this->mouseButtonState[GET_MOUSE_KEYCODE] = true;
+                }
+                else { this->mouseButtonState.insert({GET_MOUSE_KEYCODE, true}); }
+                break;
+#if false       
+                switch (this->events.button.button)
+                {
+                    case SDL_BUTTON_LEFT:   // 鼠标左键按下
+                        break;
+                    case SDL_BUTTON_RIGHT:  // 鼠标右键按下
+                        break;
+                    case SDL_BUTTON_MIDDLE: // 鼠标中键按下
+                        break;
+                    case SDL_BUTTON_X1:     // 鼠标侧键 1 按下
+                        break;
+                    case SDL_BUTTON_X2:     // 鼠标侧键 2 按下
+                        break;
+                    /**
+                     * 鼠标未知键位按下（像高级的鼠标会有很多的侧键，SDL 对它们似乎没做映射），
+                     * 当然大部分情况也用不到不是吗（喜）
+                    */
+                    default:    
+                        break;
+                }
+#endif      
+            // 记录鼠标的某个按键回弹
+            case SDL_MOUSEBUTTONUP:
+                this->mouseButtonState[GET_MOUSE_KEYCODE] = false;
+                break;
+#if false
+                switch (this->events.button.button)
+                {
+                    case SDL_BUTTON_LEFT:   // 鼠标左键回弹
+                        break;
+                    case SDL_BUTTON_RIGHT:  // 鼠标右键回弹
+                        break;
+                    case SDL_BUTTON_MIDDLE: // 鼠标中键回弹
+                        break;
+                    case SDL_BUTTON_X1:     // 鼠标侧键 1 回弹
+                        break;
+                    case SDL_BUTTON_X2:     // 鼠标侧键 2 回弹
+                        break;
+                    default:    
+                        break;
+                }
+#endif
+            /** 
+             * 记录鼠标滚轮的运动（一般的鼠标仅支持滚轮的垂直滚动），
+             * 但有一些专业鼠标支持水平滚动（比如 罗技 MX Master 3S，它的水平滚轮在侧面），
+             * 所以此处忽略鼠标滚轮水平滚动的事件。
+             * 
+             * （由于项目目前用不到鼠标滚轮，这部分暂时不扩展）
+            */
+            case SDL_MOUSEWHEEL:
+                // 鼠标滚轮向上滚动
+                if (this->events.wheel.y > 0) {
+                    
+                // 鼠标滚轮向下滚动
+                } else if (this->events.wheel.y < 0) {
+
+                }
                 break;
             
-            case SDL_KEYUP:
-                this->keyboardState.at(CODE) = false;
+            // 记录鼠标的运动（把坐标赋值并改变标志位即可）
+            case SDL_MOUSEMOTION:
+
+                mouseMotionState = true;
+
+                this->mousePosition.x = this->events.motion.x;
+                this->mousePosition.y = this->events.motion.y;
                 break;
 
             default:
                 break;
+        }
+
+        if (!isMouseMotionless()) { 
+            mouseMotionState = false;
         }
     }
 }
