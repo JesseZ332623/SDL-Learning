d08cd68fc83a355c2cc476c6e316550f1427d8c6
Jesse <3191955858@qq.com>
2024-09-22 16:48:35 +0800

    第十五课：游戏控制器事件

--- src/15/gameController.cpp
+++ src/15/gameController.cpp
@@ -1,10 +1,31 @@
 #include "../../SDLTemplateCode/include/SDLTemplate.hpp"
 
-#define FPS 165
+#define FPS 165     // 每秒钟帧数
+
+// 算圆的面积
+#define GET_CIRCLE_AREA(radius) ((M_PI) * (radius) * (radius))
+
+// 算当前摇杆位置与摇杆原点的距离
+#define GET_ROKER_DISTANCE(rokerPos) (                                  \
+    std::sqrt(std::pow(rokerPos.x, 2) + std::pow(rokerPos.y , 2))    \
+)
+
+/**
+ * @brief 根据摇杆所处的位置，调整纹理移动的速度。
+ *  
+ * @param __speed               速度值
+ * @param __currentRokerPos     以当前摇杆位置与摇杆原点为半径所构成的圆的面积
+ * @param __speedShown          需要显示的速度信息
+ * @param __render              渲染器
+*/
+void speedControl(
+    int & __speed, const float __currentRokerPos, 
+    FontsTexture & __speedShown, SDL_Renderer * __render
+);
 
 int WinMain(int argc, char const *argv[])
 {
-    SystemInit::WindowSize windowSize = {1500, 1000};
+    SystemInit::WindowSize windowSize = {1800, 1000};
     SystemInit sysInit(windowSize, "15 SDL Game Controller");
 
     EventsControl events;
@@ -16,14 +37,18 @@ int WinMain(int argc, char const *argv[])
 
     FontsTexture rokersPosShown;
     FontsTexture arrowAngleShown;
+    FontsTexture speedShown;
     TextureImage arrow;
 
     rokersPosShown.open("../fonts/Consolas-Regular.ttf", 20);
     arrowAngleShown.open("../fonts/Consolas-Regular.ttf", 20);
+    speedShown.open("../fonts/Consolas-Regular.ttf", 20);
     arrow.load("../img/12/arrow.png", {0, 0xFF, 0XFF, 0XFF}, SDL_TRUE, sysInit.getRenderer());
 
     SDL_Point arrowPos = {50, 50};
-    int speed = 5;
+
+    int speed = 0;      // 起始速度
+
     TextureImage::FilpAttribution flip = {
         0, {arrow.getRenderPosition().w / 2, arrow.getRenderPosition().h / 2}, SDL_FLIP_HORIZONTAL};
 
@@ -48,6 +73,8 @@ int WinMain(int argc, char const *argv[])
 
         arrowAngleShown.render(0, 30, sysInit.getRenderer());
 
+        speedShown.render(0, 65, sysInit.getRenderer());
+
 #if true
         if (events.getRockersPosition().size() != 0)
         {
@@ -57,19 +84,20 @@ int WinMain(int argc, char const *argv[])
             );
 
             arrowAngleShown.load(
-                "Arrow angle: " + std::to_string(flip.angle) + 
+                "Arrow angle: " + std::to_string(std::abs(flip.angle)) + 
                 " Arrow Position: (" + std::to_string(arrowPos.x) + ", " + std::to_string(arrowPos.y) + ")",
                 {0, 0, 0, 0xFF}, sysInit.getRenderer()
             );
 
-            arrowPos.x += events.getRockersPosition()[0].leftRokerPos.x * speed;
-            arrowPos.y += events.getRockersPosition()[0].leftRokerPos.y * speed;
+            SDL_FPoint leftRokerPos = events.getRockersPosition()[0].leftRokerPos;
+            float currentRokerRange = GET_CIRCLE_AREA(GET_ROKER_DISTANCE(leftRokerPos));
+            
+            speedControl(speed, std::abs(currentRokerRange), speedShown, sysInit.getRenderer());
 
-            flip.angle = std::atan2(
-                            events.getRockersPosition()[0].leftRokerPos.y,
-                            events.getRockersPosition()[0].leftRokerPos.x
-                        ) * (180.0F / M_PI);
+            arrowPos.x += leftRokerPos.x * speed;
+            arrowPos.y -= leftRokerPos.y * speed;
 
+            flip.angle = -std::atan2(leftRokerPos.y, leftRokerPos.x) * (180.0F / M_PI);
 
             // 调整角度到 0 到 360 度之间
             if (flip.angle < 0.0F) { flip.angle += 360.0F; }
@@ -96,4 +124,44 @@ int WinMain(int argc, char const *argv[])
     }
 
     return EXIT_SUCCESS;
+}
+
+void speedControl(
+    int & __speed, const float __currentRokerPos, 
+    FontsTexture & __speedShown, SDL_Renderer * __render
+)
+{
+    const float slowRokerRange      = GET_CIRCLE_AREA(0.45F);
+    const float mediumRokerRange    = GET_CIRCLE_AREA(0.85F);
+    const float fastRokerRange      = GET_CIRCLE_AREA(1.00F);
+
+    if ((__currentRokerPos == 0.0F))
+    {
+        __speed = 0;
+        __speedShown.load(
+            "[STOP] = " + std::to_string(__speed) + " Px / Frame", 
+            {0, 0, 0, 0xFF}, __render
+        );
+    }
+    if ((__currentRokerPos > 0.0F) && (__currentRokerPos <= slowRokerRange)) { 
+        __speed = 3;
+        __speedShown.load(
+            "[LOW SPEED] = " + std::to_string(__speed) + " Px / Frame", 
+                {0, 0, 0, 0xFF}, __render
+            );
+    }
+    if ((__currentRokerPos > slowRokerRange) && (__currentRokerPos <= mediumRokerRange)) {
+        __speed = 5;
+        __speedShown.load(
+            "[MEDIUM SPEED] = " + std::to_string(__speed) + " Px / Frame", 
+            {0, 0, 0, 0xFF}, __render
+        );
+    }
+    if ((__currentRokerPos > mediumRokerRange) && (__currentRokerPos <= fastRokerRange)) { 
+        __speed = 7;
+        __speedShown.load(
+            "[HEIGH SPEED] = " + std::to_string(__speed) + " Px / Frame", 
+            {0, 0, 0, 0xFF}, __render
+        );
+    }
 }
