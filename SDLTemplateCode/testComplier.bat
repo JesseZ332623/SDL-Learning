755a059cec7e2df612c043042268f28be8220744
Jesse <3191955858@qq.com>
2024-09-25 23:20:12 +0800

    SDL 开发模板库测试用例编译脚本

--- SDLTemplateCode/testComplier.bat
+++ SDLTemplateCode/testComplier.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM 源文件路径所在
-SET SourceCode="./SDLTemplateCodeTest.cpp"
+SET SourceCode="./SDLTemplateCodeTest3.cpp"
 
 REM SDL2 接口路径所在 
 SET SDLHeadFile="F:\SDL\SDL2\include"
@@ -17,6 +17,12 @@ SET SDLTTFHeadFile="F:\SDL\SDL2_ttf\include"
 
 REM  SDL_GFX 接口路径所在
 SET SDL_GFXHeadFile="F:\SDL\SDL_gfx\include"
+
+REM SDL_Mixer 接口路径所在
+SET SDL_MixerHeadFile="F:\SDL\SDL2_mixer\include"
+
+REM libsndfile 音频文件元数据读取库接口路径所在
+SET LIBSNDFILE_HeadFile="F:\libsndfile-1.2.2-win64\include"
 
 REM PCG 随机数生成器的接口路径所在
 SET PCGHeadFile="F:\pcg-c-basic\include"
@@ -44,9 +50,17 @@ REM SDL_TTF 的库文件，库名
 SET SDLTTF_LibPath="F:\SDL\SDL2_ttf\lib\x64"
 SET SDLTTF_LibName="SDL2_ttf"
 
+REM SDL_Mixer 的库文件，库名
+SET SDLMixer_LibPath="F:\SDL\SDL2_mixer\lib\x64"
+SET SDLMixer_LibName="SDL2_mixer"
+
 REM SDL 开发模板库的路径所在
 SET SDLTemplate_LibPath="..\..\SDLTemplateCode\lib"
 SET SDLTemplate_LibName="SDLTemplateCode"
+
+REM libsndfile 音频文件元数据读取库路径和库名所在
+SET LIBSNDFILE_LibPath="F:\libsndfile-1.2.2-win64\lib"
+SET LIBSNDFILE_LibName="sndfile"
 
 REM PCG 随机数生成器的库文件路径所在
 SET PCG_LibPath="F:\pcg-c-basic\lib\pcg_basic.o"
@@ -58,10 +72,11 @@ REM FMT 日志库的库文件路径所在
 SET FMT_LibPath="F:\fmt\lib\libfmtd.a"
 
 REM 可执行文件路径
-SET ExecutionFilePath="../../bin/SDLTemplateCodeTest.exe"
+SET ExecutionFilePath="../../bin/SDLTemplateCodeTest3.exe"
 
-SET ALL_INCLUDE=-I%SDLHeadFile% -I%SDLImageHeadFile% -I%SDL_NetHeadFile% -I%SDL_GFXHeadFile% -I%SDLTTFHeadFile%
-SET ALL_LIB=-L%SDLTemplate_LibPath% -L%SDL_LibPath% -L%SDLImage_LibPath% -L%SDLNet_LibPath% -L%SDLGFX_LibPath% -L%SDLTTF_LibPath%
-SET ALL_LIBNAME=-l%SDLTemplate_LibName% -l%SDL_LibName% -l%SDLImage_LibName% -l%SDLNet_LibName% -l%SDLGFX_LibName% -l%SDLTTF_LibName%
+SET ALL_INCLUDE=-I%SDLHeadFile% -I%SDLImageHeadFile% -I%SDL_NetHeadFile% -I%SDLTTFHeadFile% -I%SDL_MixerHeadFile% -I%LIBSNDFILE_HeadFile%
+SET ALL_LIB=-L%SDLTemplate_LibPath% -L%SDL_LibPath% -L%SDLImage_LibPath% -L%SDLNet_LibPath% -L%SDLTTF_LibPath% -L%SDLMixer_LibPath% -L%LIBSNDFILE_LibPath%
+SET ALL_LIBNAME=-l%SDLTemplate_LibName% -l%SDL_LibName% -l%SDLImage_LibName% -l%SDLNet_LibName% -l%SDLTTF_LibName% -l%SDLMixer_LibName% -l%LIBSNDFILE_LibName%
+
 
 g++ %SourceCode% %FMT_LibPath% %ALL_INCLUDE% %ALL_LIB% -o %ExecutionFilePath% -O3 -Wall %ALL_LIBNAME% -std=c++23
