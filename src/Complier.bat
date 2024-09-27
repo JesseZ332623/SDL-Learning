a2d04b6adf944a2010794c4602ade020394355c1
Jesse <3191955858@qq.com>
2024-09-27 19:53:39 +0800

    编译脚本

--- src/Complier.bat
+++ src/Complier.bat
@@ -65,7 +65,7 @@ REM FMT 日志库的库文件路径所在
 SET FMT_LibPath="F:\fmt\lib\libfmtd.a"
 
 REM 可执行文件路径
-SET ExecutionFilePath="../../bin/16.exe"
+SET ExecutionFilePath="../../bin/17.exe"
 
 SET ALL_INCLUDE=-I%SDLHeadFile% -I%SDLImageHeadFile% -I%SDL_NetHeadFile% -I%SDL_GFXHeadFile% -I%SDLTTFHeadFile% -I%SDL_MixerHeadFile%
 SET ALL_LIB=-L%SDLTemplate_LibPath% -L%SDL_LibPath% -L%SDLImage_LibPath% -L%SDLNet_LibPath% -L%SDLGFX_LibPath% -L%SDLTTF_LibPath% -L%SDLMixer_LibPath%
